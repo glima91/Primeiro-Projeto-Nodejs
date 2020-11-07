07beaa38461e4997a247a6895698fb5f1a964f14
gustavo lima <gustavolima@pop-os.localdomain>
2020-11-07 10:28:23 -0300

    commit inicial

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,13 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface TransactionDTO {
+  title: string,
+	value: number,
+	type: "income" | "outcome"
+}
+
+
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -15,14 +22,45 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction: TransactionDTO) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+         income: 0,
+         outcome: 0,
+         total: 0,
+      },
+    );
+
+      balance.total = balance.income - balance.outcome;  
+
+      return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, type, value } : TransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction( { title,value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
